ec713b08e557ae5f225925540b3782db491fccb0
a-hagi613 <abdullahi.hagi98@gmail.com>
2022-12-05 10:23:14 -0500

    finished 12.1

--- feed-app/src/app/App.js
+++ feed-app/src/app/App.js
@@ -1,102 +1,92 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import "./App.css";
-import { Route, Routes } from "react-router-dom";
-import { Layout, Menu, Breadcrumb } from "antd";
-
-import Home from "../user/home/Home";
-// import Pic from "../util/img/8271062_5551.jpg";
-import Headers from "../components/header/Header.js";
+import { Route, Routes, Navigate } from "react-router-dom";
+import { Layout } from "antd";
+import Header from "../components/header/Header.js";
 import Login from "../user/login/Login";
 import Signup from "../user/Signup/Signup";
 import Profile from "../user/profile/Profile";
+import Home from "../user/home/Home";
 import MyFeeds from "../user/myFeeds/MyFeeds";
+import LoadingIndicator from "../components/LoadingIndicator/LoadingIndicator";
+import { AppContext } from "../context/applicationContext";
 
-// import Home from "../components/homePage/Home";
+const { Content } = Layout;
 
-const { Header, Content, Footer } = Layout;
 const App = () => {
+  const appContext = useContext(AppContext);
+  const userData = appContext.getSession();
+
   const [currentUser, setCurrentUser] = useState(null);
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState(null);
 
-  return (
-    <Layout className="layout" style={{ background: "white" }}>
-      {/* <Header>
-        <div className="logo" />
-        <Menu
-          theme="dark"
-          mode="horizontal"
-          defaultSelectedKeys={["2"]}
-          items={new Array(7).fill(null).map((_, index) => {
-            const key = index + 1;
-            return {
-              key,
-              label: `nav ${key}`,
-            };
-          })}
-        />
-      </Header> */}
-      <Content className="app-content">
-        <div className="container">
-          <Routes>
-            {/* <Route exact path="/" element={<Home />} /> */}
+  useEffect(() => {
+    if (userData) {
+      setCurrentUser(userData);
+      setIsAuthenticated(true);
+    } else {
+      setIsAuthenticated(false);
+    }
+  }, [userData]);
 
-            <Route
-              exact
-              path="/"
-              element={
-                <Headers>
-                  <Home currentUser={currentUser} />
-                </Headers>
-              }
-            />
-            <Route
-              exact
-              path="/login"
-              element={
-                <Login
-                  currentUser={currentUser}
-                  isAuthenticated={isAuthenticated}
-                />
-              }
-            />
-            <Route
-              exact
-              path="/signup"
-              element={
-                <Signup
-                  currentUser={currentUser}
-                  isAuthenticated={isAuthenticated}
-                />
-              }
-            />
-            <Route
-              exact
-              path="/profile"
-              element={
-                <Headers>
-                  <Profile currentUser={currentUser} />
-                </Headers>
-              }
-            />
+  if (isAuthenticated === null) return <LoadingIndicator fullPage />;
 
-            <Route
-              exact
-              path="/myFeeds"
-              element={
-                <Headers>
-                  <MyFeeds currentUser={currentUser} />
-                </Headers>
-              }
-            />
-          </Routes>
-        </div>
-      </Content>
-      {/* <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />
-      <Footer style={{ textAlign: "center", background: "white" }}>
-        FeedApp ©2022 Created by Team 2
-      </Footer> */}
-    </Layout>
-  );
+  if (isAuthenticated === false) {
+    return (
+      <Layout className="layout">
+        <Content className="app-content">
+          <div className="container">
+            <Routes>
+              <Route path="*" element={<Navigate to="/login" replace />} />
+              <Route exact path="/login" element={<Login />} />
+              <Route exact path="/signup" element={<Signup />} />
+            </Routes>
+          </div>
+        </Content>
+      </Layout>
+    );
+  } else {
+    return (
+      <Layout className="layout">
+        <Content className="app-content">
+          <div className="container">
+            <Routes>
+              <Route path="*" element={<Navigate to="/" replace />} />
+              <Route
+                exact
+                path="/profile"
+                element={
+                  <Header>
+                    <Profile currentUser={currentUser} />
+                  </Header>
+                }
+              />
+
+              <Route
+                exact
+                path="/"
+                element={
+                  <Header>
+                    <Home currentUser={currentUser} />
+                  </Header>
+                }
+              />
+
+              <Route
+                exact
+                path="/myFeeds"
+                element={
+                  <Header>
+                    <MyFeeds currentUser={currentUser} />
+                  </Header>
+                }
+              />
+            </Routes>
+          </div>
+        </Content>
+      </Layout>
+    );
+  }
 };
 
 export default App;
